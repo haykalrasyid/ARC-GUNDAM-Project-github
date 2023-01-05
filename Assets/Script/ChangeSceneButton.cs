d767026d4a59ce7579bc659035e9cdcbd1ee8e2b
haykalrasyid <mhaykalrasyidalwini@gmail.com>
2023-01-05 17:59:38 +0700

    Initial commit
    With 3D model

---
+++ Assets/Script/ChangeSceneButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneButton : MonoBehaviour
+{
+
+    public void LoadScene(string SelectPilot)
+    {
+        SceneManager.LoadScene(SelectPilot);
+    }
+}
